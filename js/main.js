972b7a0904248439759086b7b29352b7ac3beb3f
Steve Randles <revesandles@gmail.com>
2017-01-31 20:11:41 -0500

    Items 3, 4, and 5
    Finished most of 3 all of 4 and all of 5. Had trouble with 3 closing the
    video when it had ended.

--- js/main.js
+++ js/main.js
@@ -4,9 +4,43 @@ $( window ).load(function() {
 
 	$.getJSON( "/js/data.json", function( data ) {
 	  
-	  $.each( data, function( key, val ) {
-	   var item = "<div id='" + key + "' class='video'><img class='thumbnail' src='" + val.thumbnail + "'/><h1>" + val.label + "</h1></div>" ;
-	    $('.container').append(item);
-	  });
+		$.each( data, function( key, val ) {
+			var item = "<div id='" + key + "' class='video'><img class='thumbnail' src='" + val.thumbnail + "'/><h1>" + val.label + "</h1></div>" ;
+			$('.container').append(item);
+
+			$(".container").on("click", "#" + key, function() {
+				var views = localStorage.getItem(val.label);
+				if(views === null || views === "NaN"){
+					views = 1
+				}
+				else{
+					views++;
+				}
+				localStorage.setItem(val.label, views);
+
+				var vid = "<div class='videoContainer animateIn'><video class='videoplayer' autoplay><source src='" + val.video +"' type='video/mp4'></video></div>";
+				$('.video-library').append(vid);				
+			});
+
+		});
+
+		$(".video-library").on("click", ".videoContainer", function() {
+			closeVideoContainer();
+		});
+
+		$(".video-library").on("ended", ".videoContainer.videoplayer", function(){
+			closeVideoContainer();
+		});
+
 	});
 });
+
+ function closeVideoContainer() {
+ 	$(".videoContainer").removeClass("animateIn");
+	$(".videoContainer").addClass("animateOut");
+
+	$(".videoContainer").on('webkitAnimationEnd oanimationend msAnimationEnd animationend',  function(e) {
+		$(".videoContainer").remove();
+	});
+ }
+
